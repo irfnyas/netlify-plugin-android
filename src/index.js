a9466b532e16f1b6b364f10f7e0c6af01cf62c32
Irfan Yas <irfanyasiras@gmail.com>
2024-10-29 06:05:06 +0000

    Flutter android

--- src/index.js
+++ src/index.js
@@ -1,34 +1,62 @@
 const flutterPath = process.env['HOME'] + '/flutter'
 const flutterBinPath = flutterPath + '/bin'
+const androidSdkPath = process.env['HOME'] + '/android-sdk'
+const javaPath = process.env['HOME'] + '/java'
 
 /* eslint-disable no-unused-vars */
 export const onPreBuild = async function ({
   inputs,
-  // Core utilities
-  utils: {
-    // Utility to report errors.
-    // See https://github.com/netlify/build#error-reporting
-    build,
-    // Utility to display information in the deploy summary.
-    // See https://github.com/netlify/build#logging
-    status,
-    // Utility for caching files.
-    // See https://github.com/netlify/build/blob/master/packages/cache-utils#readme
-    cache,
-    // Utility for running commands.
-    // See https://github.com/netlify/build/blob/master/packages/run-utils#readme
-    run,
-  },
+  utils: { build, status, cache, run },
 }) {
   const targetChannel = inputs.channel || 'stable'
 
+  // Install Java
+  console.log('☕️ Installing Java JDK')
+  await run('wget', [
+    'https://download.oracle.com/java/17/latest/jdk-17_linux-x64_bin.tar.gz',
+    '-O',
+    'java.tar.gz',
+  ])
+  await run('mkdir', ['-p', javaPath])
+  await run('tar', ['-xzf', 'java.tar.gz', '-C', javaPath, '--strip-components=1'])
+  process.env['JAVA_HOME'] = javaPath
+  process.env['PATH'] = process.env['PATH'] + ':' + javaPath + '/bin'
+  console.log('✅ Java JDK installed')
+
+  // Install Android SDK
+  console.log('📱 Downloading Android SDK')
+  await run('mkdir', ['-p', androidSdkPath])
+  await run('wget', [
+    'https://dl.google.com/android/repository/commandlinetools-linux-8092744_latest.zip',
+    '-O',
+    'cmdline-tools.zip',
+  ])
+  await run('unzip', ['cmdline-tools.zip', '-d', androidSdkPath])
+  await run('mv', [
+    `${androidSdkPath}/cmdline-tools`,
+    `${androidSdkPath}/cmdline-tools/latest`,
+  ])
+  process.env['ANDROID_HOME'] = androidSdkPath
+  process.env['PATH'] =
+    process.env['PATH'] + ':' + androidSdkPath + '/cmdline-tools/latest/bin:' + androidSdkPath + '/platform-tools'
+  console.log('✅ Android SDK installed')
+
+  console.log('🔧 Setting up Android SDK packages')
+  await run('yes |', [
+    `${androidSdkPath}/cmdline-tools/latest/bin/sdkmanager`,
+    `"platform-tools"`,
+    `"platforms;android-30"`,
+    `"build-tools;30.0.3"`,
+  ])
+
+  // Install Flutter
   console.log('⚡️ Downloading Flutter Stable SDK')
   await run('git', [
     'clone',
     'https://github.com/flutter/flutter.git',
     '-b',
     targetChannel,
-    process.env['HOME'] + '/flutter',
+    flutterPath,
   ])
   console.log('✅ Flutter SDK downloaded')
 
@@ -38,5 +66,5 @@ export const onPreBuild = async function ({
   console.log('🔨 Running flutter doctor')
   await run('flutter', ['doctor'])
 
-  status.show({ summary: 'Flutter SDK installed' })
+  status.show({ summary: 'Flutter, Java, and Android SDK installed' })
 }
